b0f8fb15924e742e34651a9b3db1bf09708ba276
Dipta Das <dipta670@gmail.com>
2020-06-08 10:06:06 -0500

    Add initial error structure

---
+++ src/main/java/ires/baylor/edu/logerrors/model/LogError.java
@@ -0,0 +1,13 @@
+package ires.baylor.edu.logerrors.model;
+
+import java.util.List;
+
+public class LogError {
+    String source; // path to log file
+    int lineNumber; // starting line number within the source file
+    String errorMessage;
+    List<String> traceBacks;
+    LogError nestedError;
+
+    // TODO: StackOverflow, GitHub links
+}
