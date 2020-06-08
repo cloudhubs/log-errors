c50d41b8c816e54e312c1af9e92236bdb55431be
Dipta Das <dipta670@gmail.com>
2020-06-08 09:54:11 -0500

    Initial spring boot project setup

---
+++ src/test/java/ires/baylor/edu/logerrors/LogErrorsApplicationTests.java
@@ -0,0 +1,13 @@
+package ires.baylor.edu.logerrors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogErrorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
