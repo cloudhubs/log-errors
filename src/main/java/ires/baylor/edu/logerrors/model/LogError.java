4c19f65c3e8729c3dd026bb5e66f0f4e54cc4498
Dipta Das <dipta670@gmail.com>
2020-06-08 10:25:45 -0500

    Add lombok annotations to models

--- src/main/java/ires/baylor/edu/logerrors/model/LogError.java
+++ src/main/java/ires/baylor/edu/logerrors/model/LogError.java
@@ -1,7 +1,14 @@
 package ires.baylor.edu.logerrors.model;
 
+import lombok.*;
+
 import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class LogError {
     String source; // path to log file
     int lineNumber; // starting line number within the source file
