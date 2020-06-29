7a0bfbc6d14cfa7f1f4dc3f57b504e6002cf966d
Elizabeth Brighton <egbrighton@gmail.com>
2020-06-29 10:04:40 -0700

    Missed more merging issues

--- src/main/java/ires/baylor/edu/logerrors/controller/LogMatcherController.java
+++ src/main/java/ires/baylor/edu/logerrors/controller/LogMatcherController.java
@@ -1,12 +1,8 @@
 package ires.baylor.edu.logerrors.controller;
 
-<<<<<<< HEAD
-=======
-import ires.baylor.edu.logerrors.matcher.ScraperConnector;
->>>>>>> master
+import ires.baylor.edu.logerrors.matcher.MatcherControllerParameters;
 import ires.baylor.edu.logerrors.matcher.ScraperObject;
 import ires.baylor.edu.logerrors.matcher.StackOverflowScraperMatcher;
-import ires.baylor.edu.logerrors.matcher.MatcherControllerParameters;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,23 +10,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.util.ArrayList;
 import java.util.List;
+
 
 
 @Slf4j
 @RestController
 public class LogMatcherController {
-<<<<<<< HEAD
-    @PostMapping("/matcher")
-    public ResponseEntity<?> resolveErrors(@RequestBody MatcherControllerParameters request)  {
-=======
+
     @GetMapping("/matcher")
-    public ResponseEntity<?> resolveErrors(@RequestBody TempControllerParametersNoDB request)  {
->>>>>>> master
+    public ResponseEntity<?> resolveErrors(@RequestBody MatcherControllerParameters request)  {
+
         log.info("Request: " + request);
         try {
             List<ScraperObject> results = StackOverflowScraperMatcher.matchLog(request);
